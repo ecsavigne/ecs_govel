839dfaf0affd756c4d9427b21294981d91b3b044
ecsavigne <36251092+ecsavigne@users.noreply.github.com>
2025-01-29 11:36:32 -0300

    Update RepositoryKernel.go
    Agregando configuracion de ModelKernel en Estructura RepositoryKerne

--- app/repositories/RepositoryKernel.go
+++ app/repositories/RepositoryKernel.go
@@ -1,18 +1,35 @@
 package repositories
 
+import "ecs_govel/app/model"
+
+
+type RepositoryType = string
+
+const (
+	RepositoriesTypeTemplate RepositoryType = "repository_type_template"
+	// Asi crear el resto
+)
+
 type Repository interface {
 	// Funciones comunes para todos los repositorios que embeben Repository y se implementan en RepositoryKernelssss
 	GetType() string
+	GetRepository() Repository
 }
 
 type RepositoryKernel struct {
-	Type string
+	_type string
+	_rep   Repository
+	_model model.ModelKernel
 }
 
 func (r *RepositoryKernel) GetType() string {
 	return r.Type
 }
 
-const (
-	RepositoriesTypeTemplate = "repository_type_template"
-)
+func (r *RepositoryKernel) GetRepository() Repository {
+	return r._rep
+}
+
+func (r *RepositoryKernel) GetModel() model.ModelKernel {
+	return r._model
+}
