5a10e9d3091bb5fdd1752418f7322749170715ce
ecsavigne <36251092+ecsavigne@users.noreply.github.com>
2025-01-29 16:33:12 -0300

    Update ModelKernel.go

--- app/model/ModelKernel.go
+++ app/model/ModelKernel.go
@@ -1,10 +1,23 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
 
 type ModelKernel interface {
-	Insert(db *gorm.DB) *gorm.DB
-	Update(db *gorm.DB) *gorm.DB
-	Delete(db *gorm.DB) *gorm.DB
+	Insert() *gorm.DB
+	Update() *gorm.DB
+	// Delete(db *gorm.DB) *gorm.DB
+	Delete() *gorm.DB
 	// Get(db *gorm.DB, id uint)
+}
+
+var SetGlobalDB = func(db_ *gorm.DB) {
+	db = db_
+}
+
+var GetGlobalDB = func() *gorm.DB {
+	return db
 }
